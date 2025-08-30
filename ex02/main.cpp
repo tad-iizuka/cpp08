2e50bf24f60172e827366eea30b2578c21583a21
tad.iizuka@gmail.com <tad-iizuka>
2025-08-30 19:44:22 +0900

    add: reverse

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tiizuka <tiizuka@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/30 12:57:03 by tiizuka           #+#    #+#             */
-/*   Updated: 2025/08/30 19:40:21 by tiizuka          ###   ########.fr       */
+/*   Updated: 2025/08/30 19:42:18 by tiizuka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,9 @@ int main(void)
 	Log::a(F, L, C_G, "size:", Log::itoa(mstack.size()), "top:", Log::itoa(mstack.top()));
 	for (MutantStack<int>::iterator it = mstack.begin(); it != mstack.end(); ++it) {
 			Log::a(F, L, C_G, "index:", Log::itoa(std::distance(mstack.begin(), it)), "value:", Log::itoa(*it));
+	}
+	for (MutantStack<int>::reverse_iterator it = mstack.rbegin(); it != mstack.rend(); ++it) {
+			Log::a(F, L, C_G, "index:", Log::itoa(std::distance(mstack.rbegin(), it)), "value:", Log::itoa(*it));
 	}
 	return (EXIT_SUCCESS);
 }
