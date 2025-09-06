d824e8ba790c9e9a5b1179882252d8efe9f6f1d7
tad-iizuka <tad.iizuka@gmail.com>
2025-09-06 15:29:16 +0900

    change: debug msg

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,13 +6,48 @@
 /*   By: tiizuka <tiizuka@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/30 12:57:03 by tiizuka           #+#    #+#             */
-/*   Updated: 2025/08/30 20:41:29 by tiizuka          ###   ########.fr       */
+/*   Updated: 2025/09/06 15:28:35 by tiizuka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "MutantStack.hpp"
 
+void subjectTest(void);
+void debug(void);
+
 int main(void)
+{
+	subjectTest();
+	debug();
+	return (EXIT_SUCCESS);
+}
+
+void subjectTest(void)
+{
+	MutantStack<int> mstack;
+	mstack.push(5);
+	mstack.push(17);
+	std::cout << mstack.top() << std::endl;
+	mstack.pop();
+	std::cout << mstack.size() << std::endl;
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+	//[...]
+	mstack.push(0);
+	MutantStack<int>::iter it = mstack.begin();
+	MutantStack<int>::iter ite = mstack.end();
+	++it;
+	--it;
+	while (it != ite)
+	{
+		std::cout << *it << std::endl;
+		++it;
+	}
+	std::stack<int> s(mstack);
+}
+
+void debug(void)
 {
 	MutantStack<int> mstack;
 	mstack.push(2);
@@ -25,17 +60,18 @@ int main(void)
 	mstack.push(8);
 	mstack.push(10);
 	Log::a(F, L, C_G, "size:", Log::itoa(mstack.size()), "top:", Log::itoa(mstack.top()));
-	for (MutantStack<int>::iter it = mstack.begin(); it != mstack.end(); ++it) {
-			Log::a(F, L, C_G, "index:",
-				Log::itoa(std::distance(mstack.begin(), it)),
-				"value:",
-				Log::itoa(*it));
+	for (MutantStack<int>::iter it = mstack.begin(); it != mstack.end(); ++it)
+	{
+		Log::a(F, L, C_G, "index:",
+					 Log::itoa(std::distance(mstack.begin(), it)),
+					 "value:",
+					 Log::itoa(*it));
 	}
-	for (MutantStack<int>::reverse_iter it = mstack.rbegin(); it != mstack.rend(); ++it) {
-			Log::a(F, L, C_G, "index:",
-				Log::itoa(std::distance(mstack.rbegin(), it)),
-				"value:",
-				Log::itoa(*it));
+	for (MutantStack<int>::reverse_iter it = mstack.rbegin(); it != mstack.rend(); ++it)
+	{
+		Log::a(F, L, C_G, "index:",
+					 Log::itoa(std::distance(mstack.rbegin(), it)),
+					 "value:",
+					 Log::itoa(*it));
 	}
-	return (EXIT_SUCCESS);
 }
